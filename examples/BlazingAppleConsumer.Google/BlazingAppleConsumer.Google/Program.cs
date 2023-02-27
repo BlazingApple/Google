c600c2f20b8b1e0ec115b0de1f29c44e5ce4f0b6
Taylor White <whitetc2@gmail.com>
2023-02-27 10:44:20 -0500

    Update license

--- examples/BlazingAppleConsumer.Google/BlazingAppleConsumer.Google/Program.cs
+++ examples/BlazingAppleConsumer.Google/BlazingAppleConsumer.Google/Program.cs
@@ -1,12 +1,14 @@
 using BlazingApple.Google.Services;
+using BlazingApple.Google.Services.Calendars;
 using BlazingAppleConsumer.Google;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazingAppleGoogle(builder.Configuration);
+builder.Services.AddScoped<CalendarRetrievalService>();
 await builder.Build().RunAsync();
